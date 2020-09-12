32fdc63742a4942d9ecbb8c895398ccec24e5ba3
GitHub Action <action@github.com>
2020-09-12 22:11:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/orangenbaumblatt/landotoolbox/MyBundle.kt
+++ src/main/kotlin/com/github/orangenbaumblatt/landotoolbox/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.orangenbaumblatt.landotoolbox
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/orangenbaumblatt/landotoolbox/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.orangenbaumblatt.landotoolbox.services
+
+import com.github.orangenbaumblatt.landotoolbox.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
